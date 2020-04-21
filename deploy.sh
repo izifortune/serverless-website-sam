e5aa6b654458bf12cdcb3132733e897471ac3e00
Fabrizio Fortunato <fortune@cortesconta.net>
2020-04-21 22:11:49 +0100

    added .gitignore and fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-sam deploy --template-file packaged.yaml --stack-name <yourwebsite> --capabilities CAPABILITY_IAM --region us-east-1",
+sam deploy --template-file packaged.yaml --stack-name <yourwebsite> --capabilities CAPABILITY_IAM --region us-east-1
